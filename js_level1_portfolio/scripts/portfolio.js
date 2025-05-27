f43e3edd9b4fe84f6137ae6f0f8ad650eaf25e3f
sungeun <cmqktk18@gmail.com>
2025-05-27 16:09:43 +0900

    프로젝트 뷰 완성

--- js_level1_portfolio/scripts/portfolio.js
+++ js_level1_portfolio/scripts/portfolio.js
@@ -19,18 +19,23 @@ thumbNailA[0].addEventListener('click', function(){
     console.log(this);//이벤트객체 자동인식하는지 확인
     console.log(this.children);//thumbNailA의 자식 img
     console.log(this.children[0].src);//콘솔 확인시 img의 배열 현식으로 확인되어 [0]을 추가, img의 속성 src를 선택한다.
-    //팝업 bg의 자식의 자식의 src확인
+    //팝업 bg의 자식(contents)의 자식(img)의 src확인
     console.log(popupBg.children[0].children[0].src);
     //img의 속성을 선택하려면 두번 들어가야 하기에 children[0] 두번 작성
-    popupShow(); //팝업 출력함수 호출
-    popupBg.children[0].children[0].src = this.children[0].src
+    popupShow(this); //팝업 출력함수 호출
     // 대입되는 위치 = 대입되는 값
 });
 
-thumbNailA[1].addEventListener('click',popupShow);
+thumbNailA[1].addEventListener('click',function(){
+    popupShow(this);
+});
 thumbNailA[2].addEventListener('click',popupShow);
-function popupShow(){
-    return popupBg.style.display = 'flex';
+function popupShow(target){
+    console.log('==================함수 시작 위치와 에러 체크');
+    //console.log(this.children[0]); //오류 나는 부부 체크 
+    popupBg.style.display = 'flex';
+    popupBg.children[0].children[0].src = target.children[0].src;
+    return;//함수 종료 표시
 }
 //3. 배경 클릭할때 팝업 숨기기
 popupBg.addEventListener('click',popupHide);
